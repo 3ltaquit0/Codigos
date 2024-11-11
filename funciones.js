249ffd954f41c9d0f2d60922b50533474e217f8e
Victor Dector Torres <eltaquito112244@gmail.com>
2024-11-11 10:46:13 -0600

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,78 @@
+// Redirección al índice si se abre sin referencia
+if (document.referrer === "") {
+  window.location.href = "../index.html"; // Adjusted path to index in CodigosMain
+}
+
+// Evitar copia de URL
+document.addEventListener("beforecopy", function (e) {
+  e.preventDefault();
+  window.location.href = "../index.html"; // Adjusted path to index in CodigosMain
+});
+
+// Función para copiar el código
+document.addEventListener("DOMContentLoaded", function () {
+  const copyButton = document.getElementById("copyButton");
+  const codeContent = document.getElementById("codigoJava");
+
+  if (copyButton && codeContent) {
+      copyButton.addEventListener("click", function () {
+          navigator.clipboard.writeText(codeContent.innerText)
+              .then(() => alert("Código copiado al portapapeles"))
+              .catch(err => alert("Error al copiar el código: " + err));
+      });
+  }
+
+// Funciones para regresar al índice
+  const backButton1 = document.getElementById("backButton1");
+  if (backButton1) {
+      backButton1.addEventListener("click", function () {
+          window.location.href = "../fundamentosdeprogramacion.html"; // Adjusted path to index in CodigosMain
+      });
+  }
+});
+
+  const backButton2 = document.getElementById("backButton2");
+  if (backButton2) {
+      backButton2.addEventListener("click", function () {
+          window.location.href = "../estructuradedatos.html"; // Adjusted path to index in CodigosMain
+      });
+}
+
+// Deshabilitar clic derecho
+document.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
+});
+
+// Deshabilitar teclas de acceso directo como F12, Ctrl+Shift+I, etc.
+document.addEventListener("keydown", function (e) {
+  if (e.key === "F12" || 
+      (e.ctrlKey && e.shiftKey && ["I", "C", "J"].includes(e.key)) || 
+      (e.ctrlKey && e.key === "u")) {
+      e.preventDefault();
+  }
+});
+
+// Desactivar arrastre de elementos
+document.addEventListener("dragstart", function (e) {
+  e.preventDefault();
+});
+
+
+// Funciones para redirigir a una página específica
+function goToWebsite(page) {
+  if (page) {
+      window.location.href = page;
+  }
+}
+
+function goToWebsite1(page) {
+  if (page) {
+      window.location.href = 'FundamentosDeProgramacion/' + page;
+  }
+}
+
+function goToWebsite2(page) {
+  if (page) {
+      window.location.href = 'EstructuraDeDatos/' + page;
+  }
+}
